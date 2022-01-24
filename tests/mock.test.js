dc163e3f79270168a03ee2fb82b53eb6682b04ab
pinck98 <bgff454@gmail.com>
2022-01-24 18:41:10 +0200

    jest mocks

---
+++ tests/mock.test.js
@@ -0,0 +1,10 @@
+const mocker = require('../mock')
+
+test('mock function', () => {
+    const mocker = jest.fn(name => `Hello ${name}`)
+    expect(mocker('osama')).toBe('Hello osama')
+    expect(mocker).toHaveBeenCalled()
+    expect(mocker).toHaveBeenCalledTimes(1)
+    expect(mocker).toHaveBeenCalledWith('osama')
+    expect(mocker).toHaveBeenLastCalledWith('osama')
+})
