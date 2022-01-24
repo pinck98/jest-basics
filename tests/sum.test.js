0cce1286468c674207460a1c2732a6e84af17b33
pinck98 <bgff454@gmail.com>
2022-01-24 13:52:53 +0200

    jest describe

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -2,23 +2,28 @@ const sum = require('../sum')
 
 // test(name , function , timeout)
 // it(name , function , timeout)
+// describe(name ,function) //*not required
 
-test('return 0', () => {
-    expect(sum()).toBe(0)
+
+describe('scheck nums sum', () => {
+    test('return 0', () => {
+        expect(sum()).toBe(0)
+    })
+
+    test('return the num', () => {
+        expect(sum(3)).toBe(3)
+    })
+
+    test('return num 1 + num 2', () => {
+        expect(sum(3,10)).toBe(13)
+    })
+
+    test('return num 1 + num 2 + num3', () => {
+        expect(sum(3,10,10)).toBe(23)
+    })
+
+    test('return the sum', () => {
+        expect(sum(3,10,10,3,10,190)).toBe(226)
+    })
 })
 
-test('return the num', () => {
-    expect(sum(3)).toBe(3)
-})
-
-test('return num 1 + num 2', () => {
-    expect(sum(3,10)).toBe(13)
-})
-
-test('return num 1 + num 2 + num3', () => {
-    expect(sum(3,10,10)).toBe(23)
-})
-
-test('return the sum', () => {
-    expect(sum(3,10,10,3,10,190)).toBe(226)
-})
